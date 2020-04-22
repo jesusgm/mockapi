8500f3d1e7c66c9006a0d3d98c94d13fbf61f567
Jesús Gándara Martínez <jesusalxen@gmail.com>
2020-04-22 20:56:43 +0200

    add changes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require("express");
+var cors = require('cors')
 var app = express();
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 
 app.get("/", function (req, res) {
@@ -13,11 +21,17 @@ const productsController = require("./controllers/products/")
 app.get("/products", productsController.get);
 app.get("/products/:id", productsController.getById);
 
+/**
+ * Emisoras de radio
+ */
+const emisorasController = require("./controllers/emisoras/");
+app.get("/emisoras", emisorasController.get);
+app.get("/emisoras/:id", emisorasController.getById);
 
 /**
  * Run app
  */
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log(`App listening on port ${port}!`);
 });
